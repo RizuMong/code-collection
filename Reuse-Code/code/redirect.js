f4e7c355fd311bfa687d6a8ec9a7f09748877cc8
Rizu Mong <rizkihaddiprayoga@gmail.com>
2025-10-09 10:11:08 +0700

    update code

--- Reuse-Code/code/redirect.js
+++ Reuse-Code/code/redirect.js
@@ -1,95 +1,89 @@
-// // Multiple Experience Redirect
-// var redirect = {
-//   type: "page",
-//   application_id: 4276,
-//   application_name: "Kepegawaian", // app name
-//   pages_id: 39214, // id page
-//   pages_name: "Direktori-Karyawan",
-//   record_id: _record.employee_id.id, // id record (jika mau redirect ke record tertentu)
-//   form_ui_id: "Ksu-NccNg", // id multiple experience
-//   tab_name: "Keahlian & Minat",
-//   "message_type": "warning" // warning/info/error
-// };
+// Multiple Experience Redirect
+var redirect = {
+  type: "page",
+  application_id: 4276,
+  application_name: "Kepegawaian", // app name
+  pages_id: 39214, // id page
+  pages_name: "Direktori-Karyawan",
+  record_id: _record.employee_id.id, // id record (jika mau redirect ke record tertentu)
+  form_ui_id: "Ksu-NccNg", // id multiple experience
+  tab_name: "Keahlian & Minat",
+  "message_type": "warning" // warning/info/error
+};
 
-// // Single Experience Manager
-// var redirect = {
-//   "type": "page",
-//   "application_id": 4249,
-//   "application_name": "Expense Management",
-//   "pages_id": 39024,
-//   "pages_name": "Pengajuan-Cuti-Karyawan",
-//   "form_ui_id": "nEEfOrSNg",
-//   "record_id": _record.settlement_id,
-//   "tab_name": "Settlement",
-//   "message_type": "success",
-// };
+// Single Experience Manager
+var redirect = {
+  "type": "page",
+  "application_id": 4249,
+  "application_name": "Expense Management",
+  "pages_id": 39024,
+  "pages_name": "Pengajuan-Cuti-Karyawan",
+  "form_ui_id": "nEEfOrSNg",
+  "record_id": _record.settlement_id,
+  "tab_name": "Settlement",
+  "message_type": "success",
+};
 
-// // Single Experience Manager If Requestor/Approver
-// var redirect = {
-//   "type": "page",
-//   "application_id": 4249,
-//   "application_name": "Expense Management",
-//   "pages_id": 39024,
-//   "pages_name": "Settlement",
-//   "form_ui_id": "nEEfOrSNg",
-//   "record_id": _record.settlement_id,
-//   "tab_name": "Settlement",
-//   "message_type": "success",
-  
-// };
+// Single Experience Manager If Requestor/Approver
+var redirect = {
+  "type": "page",
+  "application_id": 4305,
+  "application_name": "Hubungan Industrial",
+  "pages_id": 40581,
+  "pages_name": "Approve-Penghargaan",
+  "form_ui_id": "yIL6kUtqaQzir0Zl",
+  "record_id": req.id_data,
+};
 
-// var message = "Success Create Data"
+var message = "Success Create Data"
 
-// var redirect = {
-//   "application_id": 964,
-//   "application_name": "TOKO TEST",
-//   "pages_id": 8252,
-//   "pages_name": "TOKO BARU",
-//   "form_ui_id": "vb156UI",
-//   "record_id": "xxQUIOS",
-//   "tab_name": "List Detail"
-// }
+var redirect = {
+  "application_id": 964,
+  "application_name": "TOKO TEST",
+  "pages_id": 8252,
+  "pages_name": "TOKO BARU",
+  "form_ui_id": "vb156UI",
+  "record_id": "xxQUIOS",
+  "tab_name": "List Detail"
+}
 
-// // Single Redirect 
-// var redirect = {
-//   type: "page",
-//   application_id: 2873,
-//   application_name: "SMART Expense", // app name
-//   pages_id: 33033, // id page
-//   pages_name: "Create Reimbursement", // page name
-//   message: message
-// };
+// Single Redirect 
+var redirect = {
+  type: "page",
+  application_id: 2873,
+  application_name: "SMART Expense", // app name
+  pages_id: 33033, // id page
+  pages_name: "Create Reimbursement", // page name
+  message: message
+};
 
-// // Single With Redirect Another Page
-// var redirect = {
-//   "type": "url",
-//   "url": "insert_url",
-//   "target": "new_tab"
-// };
+// Single With Redirect Another Page
+var redirect = {
+  "type": "url",
+  "url": "insert_url",
+  "target": "new_tab"
+};
 
-// var message = "success redirect ke link";
+var message = "success redirect ke link";
 
 
-// // #reload component
-// var redirect = {
-//     "reload_component" : true
-//    }
+// #reload component
+var redirect = {
+    "reload_component" : true
+   }
 
 
-// // #reload data
-// var redirect = {
-//     "reload_data" : true
-//    }
+// #reload data
+var redirect = {
+    "reload_data" : true
+   }
 
-// // #reload page
-// var redirect = {
-//     "reload_page" : true
-//    }
+// #reload page
+var redirect = {
+    "reload_page" : true
+   }
 
-// // #redirect to page using comp. embed url
-// var redirect = {
-//     "param_embed_url" : "?id=10&status=success"
-// }
-
-
-var getAll = 
+// #redirect to page using comp. embed url
+var redirect = {
+    "param_embed_url" : "?id=10&status=success"
+}
